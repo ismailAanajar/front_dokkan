12bc8a12d62a0ec3269c531cb3f5c360bc9f43ed
ismail anajar <omaranajar@gmail.com>
2023-03-07 11:24:15 +0100

    fix typo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
-import '@/dokkan/styles/globals.css';
+import '@dokkan/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-
 import { store } from '../store';
 import themeConfig from '../theme';
 
